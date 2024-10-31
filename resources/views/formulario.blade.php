1a3abbf1629b1b79421bff214b77c3aff76a514d
Nicole Ricaldi <nicolericaldi@gmail.com>
2024-10-31 01:27:33 -0400

    add redirect follow to fetch request

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -311,6 +311,7 @@
 
                 const response = await fetch('{{ route('solicitante.uploadFile') }}', {
                     method: 'POST',
+                    redirect: 'follow',
                     body: formData,
                     headers: {
                         'X-CSRF-TOKEN': '{{ csrf_token() }}'
