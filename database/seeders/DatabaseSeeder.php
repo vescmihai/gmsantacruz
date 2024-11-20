2343e2376b1eff383b6b318bd55ac154ce394718
Nicole Ricaldi <nicolericaldi@gmail.com>
2024-11-19 23:05:45 -0400

    chore: add user admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,9 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Call other migrations
         $this->call([
             TipoLicenciaSeeder::class,
             RolSeeder::class,
+        ]);
+
+        // Create Super User
+        DB::table('users')->truncate();
+
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'rol_id' => '1',
+            'wallet_address' => '0xadmin',
+            'created_at' => now(),
+            'updated_at' => now()
         ]);
     }
 }
